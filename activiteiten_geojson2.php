43382cc3ad264ea1da42a36dd87baeea67e910d9
Pietervdvn <pietervdvn@gmail.com>
2018-03-10 17:42:32 +0100

    Added afdeling

--- activiteiten_geojson2.php
+++ activiteiten_geojson2.php
@@ -11,18 +11,23 @@ if ($debug) {
 	error_reporting(E_ALL);
 }
 
-$startdate = "[should be defined in GET]";
+$afdeling = "[should be defined in GET]";
+if(isset($_GET["afdeling"])) {
+	$afdeling = mysqli_real_escape_string($conn_drupal, $_GET["afdeling"]);
+}
+else die("?afdeling=.. should be defined");
+
+
+$startdate = "20100101";
 if(isset($_GET["startdate"])) {
 	$startdate = mysqli_real_escape_string($conn_drupal, $_GET["startdate"]);
 }
-else die("?startdate=.. should be defined");
 
 
-$sql_query="SELECT AVG(field_activiteit_locatie_lat) as lat_center,AVG(field_activiteit_locatie_lon) as lon_center, tAlgemeen.organisator_19 as organiserende_afdeling
+$sql_query="SELECT field_activiteit_locatie_lat as lat_center, field_activiteit_locatie_lon as lon_center, tAlgemeen.organisator_19 as organiserende_afdeling
 	FROM field_data_field_activiteit_civicrm_event tEvent
 	INNER JOIN field_revision_field_activiteit_locatie tLocatie ON tEvent.entity_id=tLocatie.entity_id
-   LEFT JOIN jnet1980_test_civicrm.civicrm_value_algemeen_8 tAlgemeen ON field_activiteit_civicrm_event_target_id=tAlgemeen.entity_id
-	GROUP BY organiserende_afdeling;";
+   LEFT JOIN jnet1980_test_civicrm.civicrm_value_algemeen_8 tAlgemeen ON field_activiteit_civicrm_event_target_id=tAlgemeen.entity_id;";
 
 
 
