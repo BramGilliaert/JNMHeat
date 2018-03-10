e214fc6b41eede0068674c9b1895f68d946d145b
Pietervdvn <pietervdvn@gmail.com>
2018-03-10 15:47:09 +0100

    Testing geocenter

--- activiteiten_geocenter.php
+++ activiteiten_geocenter.php
@@ -20,7 +20,7 @@ else die("?startdate=.. should be defined");
 
 $sql_query="SELECT tEvent.entity_id,field_activiteit_locatie_lat as lat,field_activiteit_locatie_lon as lon, tAlgemeen.organisator_19
 	FROM field_data_field_activiteit_civicrm_event tEvent
-	LEFT JOIN field_revision_field_activiteit_locatie tLocatie ON tEvent.entity_id=tLocatie.entity_id
+	INNER JOIN field_revision_field_activiteit_locatie tLocatie ON tEvent.entity_id=tLocatie.entity_id
    LEFT JOIN jnet1980_test_civicrm.civicrm_value_algemeen_8 tAlgemeen ON field_activiteit_civicrm_event_target_id=tAlgemeen.entity_id;";
 
 
