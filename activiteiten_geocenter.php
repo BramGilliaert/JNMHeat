1e69ace6fd97591099dd26c5f3d3b2b706d3a1a1
Pietervdvn <pietervdvn@gmail.com>
2018-03-10 15:46:06 +0100

    Testing geocenter

--- activiteiten_geocenter.php
+++ activiteiten_geocenter.php
@@ -18,8 +18,7 @@ if(isset($_GET["startdate"])) {
 else die("?startdate=.. should be defined");
 
 
-$sql_query="SELECT tEvent.entity_id,field_activiteit_adres_thoroughfare,field_activiteit_adres_locality,field_activiteit_adres_premise,field_activiteit_adres_postal_code,
-	field_activiteit_locatie_lon,field_activiteit_locatie_lat, tAlgemeen.organisator_19, categorie_20, leeftijdscategorie_21
+$sql_query="SELECT tEvent.entity_id,field_activiteit_locatie_lat as lat,field_activiteit_locatie_lon as lon, tAlgemeen.organisator_19
 	FROM field_data_field_activiteit_civicrm_event tEvent
 	LEFT JOIN field_revision_field_activiteit_locatie tLocatie ON tEvent.entity_id=tLocatie.entity_id
    LEFT JOIN jnet1980_test_civicrm.civicrm_value_algemeen_8 tAlgemeen ON field_activiteit_civicrm_event_target_id=tAlgemeen.entity_id;";
