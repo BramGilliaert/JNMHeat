1608af68d1f3915d55f355f6a483a383d1ce04fc
Pietervdvn <pietervdvn@gmail.com>
2018-03-11 22:13:05 +0100

    Debugging

--- activiteiten_geojson2.php
+++ activiteiten_geojson2.php
@@ -42,6 +42,7 @@ if(isset($_GET["max_duration"])) {
 $categorie = "%";
 if(isset($_GET["categorie"])) {
 	$categorie = mysqli_real_escape_string($conn_drupal, $_GET["categorie"]);
+	die($categorie);
 }
 
 
