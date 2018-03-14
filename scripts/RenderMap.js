22b052e9bff487f701c2f4b6e579a3e61b5d8a23
Emile Sonneveld <emile.jnm@gmail.com>
2018-03-14 20:47:08 +0100

    skip nationaal & werkgroepen

--- scripts/RenderMap.js
+++ scripts/RenderMap.js
@@ -173,6 +173,10 @@ function createCentersLayer(onClick){
 			var cent = afdelingCenters[i];
 
 			var afdId = cent.organiserende_afdeling;
+			// Skip werkgroepen & nationaal
+			if(afdId === '1' || afdId === '50' || afdId === '52' || afdId === '57') {
+				continue;
+			}
 			var name = id2name(afdId);
 			var pin = L.marker([cent.lat_center, cent.lon_center]);
 			var popup = L.popup({closeOnClick : false, autoClose : false, closeButton : false}).setContent("Geografisch gemiddelde van <a href='https://jnm.be/afdeling/"+name+"' target='_blank'>JNM "+name+"</a>");
