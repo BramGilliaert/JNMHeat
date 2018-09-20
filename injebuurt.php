92c85b5e2e354a2431612075f9cd72745483d518
Emile Sonneveld <contact@emilesonneveld.be>
2018-09-20 21:05:00 +0200

    Fix html->php redirect.

--- injebuurt.php
+++ injebuurt.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>In je buurt</title>
-	<link rel="stylesheet" href="vendor/leaflet/leaflet.css"/>
-	<script src="vendor/leaflet/leaflet.js"></script>
-	<script src="vendor/jquery-3.3.1.min.js"></script>
+  <meta charset="utf-8">
+  <title>In je buurt</title>
+  <link rel="stylesheet" href="vendor/leaflet/leaflet.css"/>
+  <script src="vendor/leaflet/leaflet.js"></script>
+  <script src="vendor/jquery-3.3.1.min.js"></script>
 
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
@@ -19,9 +19,9 @@
   <script src="scripts/FilterSettings.js"></script>
   <script src="scripts/RenderMap.js"></script>
  
-	<style>
-		#map{ width: 1500px; height: 1050; }
-	</style>
+  <style>
+    #map{ width: 1500px; height: 1050; }
+  </style>
 </head>
 <body>
 
